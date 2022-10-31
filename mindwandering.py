dfd474be5c554069fd65547550ad3fdf3d65c34d
Craig Baker <craig.jb@gmail.com>
2022-10-31 14:52:53 -0400

    Render text to image using PIL

--- mindwandering.py
+++ mindwandering.py
@@ -7,9 +7,31 @@
 
 from tkinter import *
 from tkinter import ttk
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-root.mainloop()
+
+import PIL.Image
+import PIL.ImageDraw
+import PIL.ImageFont
+import PIL.ImageTk
+
+
+def main():
+    root = Tk()
+    root.geometry('501x501')
+    canvas = Canvas(root, width=500, height=500)
+    canvas.pack()
+
+    image = PIL.Image.new("RGBA", (5000, 5000), (255,255,255))
+    draw = PIL.ImageDraw.Draw(image)
+    fontsize = 512
+    font = PIL.ImageFont.truetype("fonts/Merriweather/Merriweather-Black.ttf", fontsize)
+
+    draw.text((10, 0), "Hello World", (0,0,0), font=font)
+    img_resized = image.resize((500, 500), PIL.Image.Resampling.LANCZOS)
+
+    image = PIL.ImageTk.PhotoImage(img_resized)
+    imagesprite = canvas.create_image(250, 250, image=image)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
