0789b69b4afbc5881a99ec393a28f3acc94b779a
Craig Baker <craig.jb@gmail.com>
2022-12-05 17:38:25 -0500

    Updated speed options

--- mindwandering.py
+++ mindwandering.py
@@ -38,7 +38,7 @@ class MindWandering:
     def __init__(self):
         self.small_screen = False
         window_title = "Reading Application"
-        self.speed_options = [180, 200, 216, 260, 296, 312, 328, 500] # wpm
+        self.speed_options = [180, 200, 216, 260, 328, 400, 500, 550] # wpm
         if self.small_screen:
             self.image_width = 1200
             self.screen_height = 600
